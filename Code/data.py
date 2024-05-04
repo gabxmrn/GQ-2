88da26fdf5841d9512118932693b8b05d9e19cc8
Caroline KIRCH <caroline.kirch@dauphine.eu>
2024-05-04 13:00:36 +0200

    dates

--- Code/data.py
+++ Code/data.py
@@ -18,14 +18,13 @@ exogeneous_variables.index = pd.to_datetime(exogeneous_variables.index)
 exogeneous_variables = exogeneous_variables.resample('QE').mean() # Resample data to quarterly to match funds data
 exogeneous_variables.dropna(inplace=True)
 exogeneous_variables.index = exogeneous_variables.index.strftime('%Y-%m') 
-# start_date = "1980-03"
 
 # Factor variables for both factor models (Carhart &  Ferson/Schadt)
-factor = exogeneous_variables.loc[:, ['rf_rate', 'mkt_ptf', 'smb', 'hml', 'mom']] # start_date:
+factor = exogeneous_variables.loc[:, ['rf_rate', 'mkt_ptf', 'smb', 'hml', 'mom']] 
 factor['mkt_rf'] = factor['mkt_ptf'] - factor['rf_rate'] # Excess returns over risk free rate ????? 
 
 # Predictive variables of the conditionnal 4 factors model :
-predictive = exogeneous_variables.loc[:, ['1M_Tbill_yield', 'div_yield_mkt_ptf', 'term_spread', 'default_spread']]# start_date:
+predictive = exogeneous_variables.loc[:, ['1M_Tbill_yield', 'div_yield_mkt_ptf', 'term_spread', 'default_spread']]
 for column in predictive.columns:
         predictive[column] -= predictive[column].mean() # value at end of month t - mean on the period
 predictive = predictive.shift(1)  # Data shift to represent z_{t-1}
@@ -34,21 +33,21 @@ predictive.dropna(inplace=True)
 # Dates management : 
 common_dates = pd.Index(sorted(set(factor.index).intersection(set(predictive.index))))
 factor = factor.loc[common_dates, :]
-predictive = predictive.loc[common_dates, :]
+
+# 107 dates dans les variables exogènes. 
 
 # Mutual funds data
 mutual_fund = pd.read_csv("Data/mutual_funds.csv", dtype={'cusip': str})
 mutual_fund['fdate'] = pd.to_datetime(mutual_fund['fdate'])
 mutual_fund['fdate'] = mutual_fund['fdate'].apply(lambda x: x.strftime('%Y-%m'))
-mutual_fund = mutual_fund[mutual_fund['fdate'].isin(common_dates)]
-mutual_fund = mutual_fund.sort_values(by=['fundname', 'fdate'])
-mutual_fund = mutual_fund.groupby('fundname').filter(lambda x: x['rdt'].notnull().rolling(window=20).count().max() >= 20)
-mutual_fund.set_index('fdate', drop=True, inplace=True)
+mutual_fund = mutual_fund[mutual_fund['fdate'].isin(common_dates)] # Sames dates as exogeneous variables (107)
 mutual_fund.replace([np.inf, -np.inf], np.nan, inplace=True)
 mutual_fund.dropna(inplace=True)
+mutual_fund = mutual_fund.sort_values(by=['fundname', 'fdate'])
+mutual_fund = mutual_fund.groupby('fundname').filter(lambda x: x['rdt'].notnull().rolling(window=20).count().max() >= 20)
 
-# print(len(mutual_fund['fundname'].unique()))
-# print(mutual_fund)
+# print(f" nombre de fonds : {len(mutual_fund['fundname'].unique())}.") # 8525
+# print(f" nombre de dates : {len(mutual_fund['fdate'].unique())}.") # toujours 107
 
 ####################################################### DATA STATIONNARIZATION ######################################################
 
@@ -73,11 +72,12 @@ Stationnarity_Test_fund = Stationnarity_Test(mutual_fund, 10, 0.05)
 # Non-stationnary -> none 
 # print(Stationnarity_Test_fund.get_results())
 
+# 106 dates après stationnarisation
 
-######################################################### PORTFOLIO CREATION ########################################################
+# ######################################################### PORTFOLIO CREATION ########################################################
 
-mutual_fund_returns = mutual_fund.reset_index()
-mutual_fund_returns = mutual_fund_returns.pivot_table(index='fundname', columns='fdate', values='rdt', aggfunc='first')
+mutual_fund_returns = mutual_fund.pivot_table(index='fundname', columns='fdate', values='rdt', aggfunc='first')
+mutual_fund.set_index('fdate', drop=True, inplace=True)
 
 nb_funds_per_dates = mutual_fund_returns.notnull().sum()
 weighted_averages = mutual_fund_returns.sum() * (1 / nb_funds_per_dates)
@@ -86,34 +86,33 @@ weighted_portfolio = pd.DataFrame(weighted_averages, columns=['Returns'])
 weighted_portfolio["Nb funds"] = nb_funds_per_dates
 
 
-########################################################## DATES MANAGEMENT #########################################################
+# ########################################################## DATES MANAGEMENT #########################################################
 
-common_dates = pd.Index(sorted(set(weighted_portfolio.index).intersection(set(factor.index)).intersection(set(predictive.index))))
+common_dates = pd.Index(sorted(set(weighted_portfolio.index).intersection(set(factor.index)).intersection(set(predictive.index)))) # 106
 weighted_portfolio = weighted_portfolio.loc[common_dates, :]
 factor = factor.loc[common_dates, :]
 predictive = predictive.loc[common_dates, :]
 
-weighted_portfolio["Excess Returns"] = weighted_portfolio["Returns"] - factor["rf_rate"]
+weighted_portfolio["Excess Returns"] = weighted_portfolio["Returns"] - factor["rf_rate"] # Excess returns over risk free rate
 
 
+# # ########################################################################################################
+# # fig, ax1 = plt.subplots(figsize=(12, 6))
 
-# ########################################################################################################
-# fig, ax1 = plt.subplots(figsize=(12, 6))
+# # color = 'tab:blue'
+# # ax1.set_xlabel('fDate')
+# # ax1.set_ylabel('Returns', color=color)
+# # ax1.plot(weighted_portfolio.index, weighted_portfolio['Returns'], color=color)
+# # ax1.tick_params(axis='y', labelcolor=color)
+# # ax1.grid(True)
 
-# color = 'tab:blue'
-# ax1.set_xlabel('fDate')
-# ax1.set_ylabel('Returns', color=color)
-# ax1.plot(weighted_portfolio.index, weighted_portfolio['Returns'], color=color)
-# ax1.tick_params(axis='y', labelcolor=color)
-# ax1.grid(True)
+# # ax2 = ax1.twinx()
+# # color = 'tab:red'
+# # ax2.set_ylabel('Nb funds', color=color)  
+# # ax2.plot(weighted_portfolio.index, weighted_portfolio['Nb funds'], color=color, linestyle='--')
+# # ax2.tick_params(axis='y', labelcolor=color)
 
-# ax2 = ax1.twinx()
-# color = 'tab:red'
-# ax2.set_ylabel('Nb funds', color=color)  
-# ax2.plot(weighted_portfolio.index, weighted_portfolio['Nb funds'], color=color, linestyle='--')
-# ax2.tick_params(axis='y', labelcolor=color)
-
-# plt.title('Returns')
-# plt.tight_layout()
-# plt.show()
-# ########################################################################################################
+# # plt.title('Returns')
+# # plt.tight_layout()
+# # plt.show()
+# # ########################################################################################################
---
+++ Code/main.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import numpy as np
+
+from data import factor, predictive, mutual_fund, weighted_portfolio
+from regression import FactorModels
+
+
+######################################################## REGRESSIONS SUR LE PORTEFEUILLE ########################################################
+
+Portfolio_results = FactorModels(exog=factor[['mkt_rf', 'smb', 'hml', 'mom']], endog=weighted_portfolio["Excess Returns"], predictive=predictive)
+print(Portfolio_results.four_factor_model())
+print(Portfolio_results.conditional_four_factor_model())
+
+# Remarque : la je ressort tous les coeff + les p-values qsui vont avec 
+
+
+########################################################### REGRESSIONS SUR LES FONDS ###########################################################
+
+nb_funds = len(mutual_fund['fundname'].unique()) # Total number of funds 
+fund_names = np.full(nb_funds, fill_value=np.nan, dtype='object')
+fund_index = 0 # Counter for funds
+
+results = np.full((nb_funds, 5), fill_value=np.nan) 
+# Col1 = alpha model 1, Col2 = p-value model 1, Col3 = alpha model 2, Col4 = p-value model 2, Col5 = nb dates for fund
+
+for name, fund in mutual_fund.groupby('fundname'):
+    # Dates management : 
+    common_dates = set(factor.index).intersection(set(fund.index)).intersection(set(predictive.index))
+    common_dates = pd.Index(sorted(list(common_dates)))
+
+    # OLS : 
+    factor_models = FactorModels(exog = factor.loc[common_dates, ['mkt_rf', 'smb', 'hml', 'mom']], 
+                                 endog = fund.loc[common_dates, 'rdt'] - factor.loc[common_dates, 'rf_rate'] , 
+                                 predictive = predictive.loc[common_dates])
+    results_model_1 = factor_models.four_factor_model()
+    results_model_2 = factor_models.conditional_four_factor_model()
+    
+    # Résults : 
+    fund_names[fund_index] = name
+    results[fund_index, 0] = results_model_1.loc['const', 'Coeff']
+    results[fund_index, 1] = results_model_1.loc['const', 'P-value']
+    results[fund_index, 2] = results_model_2.loc['const', 'Coeff']
+    results[fund_index, 3] = results_model_2.loc['const', 'P-value']
+    results[fund_index, 4] = len(common_dates)
+    fund_index += 1
+
+full_results = pd.DataFrame(results, index=fund_names, columns = ["alpha 1", "p-value 1", "alpha 2", "p-value 2","number of data"])
+print(full_results.head(20))
+
+# Remarque : la je me concentre que sur alpha + pvalue (pour les deux modèles)
